1bd415c6ecde7adf3293a398c87e338fe02407f0
Blacdav <davidaniefoik@gmail.com>
2025-05-30 19:38:44 +0100

    set up the wallet connect feature

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import { routes } from './Routes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createNetworkConfig, SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui/client';
+import '@mysten/dapp-kit/dist/index.css';
+
+const queryClient = new QueryClient();
+
+const { networkConfig } = createNetworkConfig({
+	localnet: { url: getFullnodeUrl('localnet') },
+	devnet: { url: getFullnodeUrl('devnet') },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={routes} />
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networkConfig} defaultNetwork='devnet'>
+        <WalletProvider>
+          <RouterProvider router={routes} />
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
