11903e011b858023117e8fb47d0b16dd814028a5
legend <legendprosper2125@gmail.com>
2025-05-31 07:03:46 +0100

    commented the zkLoginButton, to allow smooth test deployment

--- src/Components/zkLoginButton.tsx
+++ src/Components/zkLoginButton.tsx
@@ -1,45 +1,45 @@
-import { useConnectWallet, useCurrentAccount, useWallets } from '@mysten/dapp-kit';
-import { isEnokiWallet } from '@mysten/enoki';
-import { type EnokiWallet } from '@mysten/enoki';
-import { type AuthProvider } from '@mysten/enoki';
+// import { useConnectWallet, useCurrentAccount, useWallets } from '@mysten/dapp-kit';
+// import { isEnokiWallet } from '@mysten/enoki';
+// import { type EnokiWallet } from '@mysten/enoki';
+// import { type AuthProvider } from '@mysten/enoki';
  
-export const ZKLoginButton = () => {
-	const currentAccount = useCurrentAccount();
-	const { connect } = useConnectWallet();
+// export const ZKLoginButton = () => {
+// 	const currentAccount = useCurrentAccount();
+// 	const { connect } = useConnectWallet();
  
-	const wallets = useWallets().filter(isEnokiWallet);
-	const walletsByProvider = wallets.reduce(
-		(map, wallet) => map.set(wallet.provider, wallet),
-		new Map<AuthProvider, EnokiWallet>(),
-	);
+// 	const wallets = useWallets().filter(isEnokiWallet);
+// 	const walletsByProvider = wallets.reduce(
+// 		(map, wallet) => map.set(wallet.provider, wallet),
+// 		new Map<AuthProvider, EnokiWallet>(),
+// 	);
  
-	const googleWallet = walletsByProvider.get('google');
-	const facebookWallet = walletsByProvider.get('facebook');
+// 	const googleWallet = walletsByProvider.get('google');
+// 	const facebookWallet = walletsByProvider.get('facebook');
  
-	if (currentAccount) {
-		return <div>Current address: {currentAccount.address}</div>;
-	}
+// 	if (currentAccount) {
+// 		return <div>Current address: {currentAccount.address}</div>;
+// 	}
  
-	return (
-		<>
-			{googleWallet ? (
-				<button
-					onClick={() => {
-						connect({ wallet: googleWallet });
-					}}
-				>
-					Sign in with Google
-				</button>
-			) : null}
-			{facebookWallet ? (
-				<button
-					onClick={() => {
-						connect({ wallet: facebookWallet });
-					}}
-				>
-					Sign in with Facebook
-				</button>
-			) : null}
-		</>
-	);
-}
+// 	return (
+// 		<>
+// 			{googleWallet ? (
+// 				<button
+// 					onClick={() => {
+// 						connect({ wallet: googleWallet });
+// 					}}
+// 				>
+// 					Sign in with Google
+// 				</button>
+// 			) : null}
+// 			{facebookWallet ? (
+// 				<button
+// 					onClick={() => {
+// 						connect({ wallet: facebookWallet });
+// 					}}
+// 				>
+// 					Sign in with Facebook
+// 				</button>
+// 			) : null}
+// 		</>
+// 	);
+// }
